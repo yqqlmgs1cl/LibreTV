32687c49d0fc431d4e1a9202003549d6407b81d3
yqqlmgs1cl <103911381+yqqlmgs1cl@users.noreply.github.com>
2025-12-12 08:44:20 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -27,6 +27,12 @@ const API_SITES = {
         name: '空内容测试源',
         adult: true
     },
+   1080zyku: {
+      api: 'https://api.1080zyku.com/inc/api_mac10.php',
+      name: 'TV-1080资源',
+      detail: 'https://api.1080zyku.com',
+     adult: true
+    },
     //ARCHIVE https://telegra.ph/APIs-08-12
 };
 
